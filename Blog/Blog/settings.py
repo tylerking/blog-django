7abb51cd448c3f309997b2be435315d9a103a21f
Tyler King <doitliketyler@gmail.com>
2022-11-19 12:37:22 -0800

    Add env variables

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -15,12 +15,15 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i*uxx@tv2@3fcmu2_-p$cel-pqk)d5588fq+p_0h_8tr-shquy'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
