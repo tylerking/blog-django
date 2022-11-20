7161bdb72336dc8702235e2d74a580c48e7dabca
Tyler King <doitliketyler@gmail.com>
2022-11-19 21:53:11 -0800

    Add static root

--- Blog/settings.py
+++ Blog/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import environ
 env = environ.Env()
 environ.Env.read_env()
 
@@ -126,6 +127,7 @@ LOGOUT_URL = 'logout'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
